84d155b316752761548c59d89d51f5cfca3b6322
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-01-08 18:30:49 +0300

    Initial Commit

---
+++ BarChartsSwiftUI/BarChartsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartsSwiftUIApp.swift
+//  BarChartsSwiftUI
+//
+//  Created by Artyom Romanchuk on 08.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct BarChartsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarChartsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarChartsSwiftUI
+//
+//  Created by Artyom Romanchuk on 08.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
